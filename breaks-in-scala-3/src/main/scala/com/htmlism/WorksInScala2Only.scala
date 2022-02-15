cb9639ede93feef0aef5481cbd953116fdb9ce27
Mark Canlas <github@htmlism.com>
2022-02-15 15:52:43 -0500

    without loop

--- breaks-in-scala-3/src/main/scala/com/htmlism/WorksInScala2Only.scala
+++ breaks-in-scala-3/src/main/scala/com/htmlism/WorksInScala2Only.scala
@@ -4,17 +4,22 @@ import cats._
 import cats.syntax.all._
 
 object WorksInScala2Only extends App {
+  val msg =
+    "this works in scala 2.12.8 ONLY"
+
   generically[List]
 
-  def generically[F[_]](implicit F: Monad[F]): F[Unit] =
-    for {
-      y <-
-        if (true)
-          F.pure {
-            println("this works in scala 2.12.8 ONLY")
-            1
-          }
-        else
-          F.unit
-    } yield ()
+  def generically[F[_]](implicit F: Monad[F]): F[Unit] = {
+    val confusingUpperBound =
+      if (true)
+        F.pure {
+          println(msg)
+          1
+        }
+      else
+        F.unit
+
+    // uses extension method `map` over `F[_]`
+    confusingUpperBound.void
+  }
 }
--- works-in-scala-3/src/main/scala/com/htmlism/WorksEverywhereDemo.scala
+++ works-in-scala-3/src/main/scala/com/htmlism/WorksEverywhereDemo.scala
@@ -3,18 +3,23 @@ package com.htmlism
 import cats._
 import cats.syntax.all._
 
-object WorksInScala2Only extends App {
+object WorksEverywhereDemo extends App {
+  val msg =
+    "this works in scala 2.12.8 and 3.1.1"
+
   generically[List]
 
-  def generically[F[_]](implicit F: Monad[F]): F[Unit] =
-    for {
-      y <-
-        if (true)
-          F.pure {
-            println("this works in scala 2.12.8 and 3.1.1")
-            1
-          }
-        else
-          F.pure(())
-    } yield ()
+  def generically[F[_]](implicit F: Monad[F]): F[Unit] = {
+    val confusingUpperBound =
+      if (true)
+        F.pure {
+          println(msg)
+          1
+        }
+      else
+        F.pure(())
+
+    // uses extension method `map` over `F[_]`
+    confusingUpperBound.void
+  }
 }
