63dce9e7d75db37658b79d7f1908835d6afbeeac
Mark Canlas <github@htmlism.com>
2022-02-15 15:25:15 -0500

    add theory

--- works-in-scala-3/src/main/scala/com/htmlism/FlatMapDemo.scala
+++ works-in-scala-3/src/main/scala/com/htmlism/FlatMapDemo.scala
@@ -6,6 +6,9 @@ import cats.syntax.all._
 object FlatMapDemo extends App {
   generically[List]
 
+  /**
+   * Is using `pure` returning `A` different than returning `F.unit`? Does it enable a LUB opportunity?
+   */
   def generically[F[_] : Monad]: F[Unit] =
     for {
       x <- println(1).pure
